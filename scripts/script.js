2cea59db8f016d1c3a7000ba412963290d693374
Александр Бандюк <Bandziuk@Lux.local>
2020-07-10 16:03:54 +0300

    1:31:21 time 88 str

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
 // в этом массиве будем сохранять все объявления
-const dataBase = [];
+// возвращаем массив после сеттера и JSON.stringify
+// используем геттер
+// распарсили то есть
+//  написали|"| []" - если получим null, запишем пустой массив
+const dataBase = JSON.parse(localStorage.getItem('awwito')) || [];
 
 // получам модальное окно
 const modalAdd = document.querySelector('.modal__add'),
@@ -16,7 +20,8 @@ const modalAdd = document.querySelector('.modal__add'),
    // получаем модальное окно товаров
    modalItem = document.querySelector('.modal__item'),
    // получаем надпись "заполните все поля"
-   modalBtnWarning = document.querySelector('.modal__btn-warning');
+   modalBtnWarning = document.querySelector('.modal__btn-warning'),
+   modalFileInput = document.querySelector('.modal__file-input');
 
 // получаем элементы формы "Подать объявление"
 // добавляем spread (...)
@@ -26,39 +31,21 @@ const modalAdd = document.querySelector('.modal__add'),
 const elementsModalSubmit = [...modalSubmit.elements]
    .filter(elem => elem.tagName !== 'BUTTON');
 
-   const closeModal = function(event) {
-      const target = event.target;
-      // закрываем попапы при клике на крестик
-      // или при клике мимо попапа
-      // this - это объект, который вызвал событие
-      // this в нашем случае - это modalAdd или modalItem
-      if (target.closest('.modal__close') || target === this) {
-         // для закрытия добавляем класс hide
-         // равно как modalAdd.classList.add('hide')
-         // и modalItem.classList.add('hide');
-         this.classList.add('hide');
-         
-         if (this === modalAdd) {
-            //очищаем форму после закрытия
-         modalSubmit.reset();
-         }
-      }
-   };
+// здесь будем хранить загруженные фото
+const infoPhoto = {};
 
-   // закрытие попапа по клику на Esc
-   const closeModalEsc = event => {
-      if (event.code === 'Escape') {
-         modalAdd.classList.add('hide');
-         modalItem.classList.add('hide');
-         modalSubmit.reset();
-         document.removeEventListener('keydown', closeModalEsc);
-      }
-   };
+// создаем localStorage для хранения введенных данных
+// (потому что сервака нет)
+// потом будем вызывать при кнопке "отправить"
+// используем сеттер
+// переводим в строку через JSON, потому что браузер не видит данные
+// без JSON было бы [Object object]
+const saveDB = () => localStorage.setItem('awwito', JSON.stringify(dataBase));
 
-   // на форму навешиваем событие
-   // разблокируем кнопку
-   // удаляем надпись "заполните все поля"
-   modalSubmit.addEventListener('input', () => {
+
+
+// перебираем формы и проверяет, что они заполнены
+const checkForm = () => {
       // every проверяет все элементы в массиве
       // если какое-то условие возвращает false, то вернет false
       // если значение пустое, то будет false
@@ -66,70 +53,102 @@ const elementsModalSubmit = [...modalSubmit.elements]
       const validForm = elementsModalSubmit.every(elem => elem.value);
       // разблокируем кнопку, когда все поля заполнены
       modalBtnSubmit.disabled = !validForm;
-      // убираем надпись "заполните все поля" при заполнении
-      if (validForm) {
-         modalBtnWarning.style.display = 'none';
-      } else {
-         modalBtnWarning.style.display = '';
-      }
-      
-      // убираем надпись "заполните все поля" - короче
-      // modalBtnWarning.style.display = validForm ? 'none' : '';
+      // убираем надпись "заполните все поля"
+      modalBtnWarning.style.display = validForm ? 'none' : '';
+};
 
-   });
+// функция закрывает окна попап
+const closeModal = event => {
+   const target = event.target;
 
-   // запрещаем перезагрузку при клике на "отправить"
-   modalSubmit.addEventListener('submit', event => {
-      event.preventDefault();
-      // в этот объект добавляем свойства и значения из name/value
-      const itemObj = {};
-      // перебираем каждый элемент в форме Подать объявление"
-      for (const elem of elementsModalSubmit) {
-         
-         itemObj[elem.name] = elem.value;
-         // это для проверки
-         // console.log(elem.name);
-         // console.log(elem.value);
-         // console.log(itemObj);
-         
-         // полученные элементы сохраняем в массив dataBase
-         dataBase.push(itemObj);
-         // и очищаем форму
+   // определяем, что кликнули на крестик
+   if (target.closest('.modal__close') ||
+      target.classList.contains('modal') ||
+      event.code === 'Escape') {
+         // закрываем любое из окон попап
+         modalAdd.classList.add('hide');
+         modalItem.classList.add('hide');
+         // удаляем обработчик события клика на Esc
+         // если не удалить, он будет срабатывать после закрытия окна
+         document.removeEventListener('keydown', closeModal);
          modalSubmit.reset();
-         console.log(dataBase);
+         checkForm();
+      }
+};
+
+// сохраняем фото
+modalFileInput.addEventListener('.change', event => {
+   const target = event.target;
+
+   // эта функция при вызове возвращает объект
+   // reader - асинхронно сможем прочитать файл
+   // не как джпег, а считать как текст
+   const reader = new FileReader();
+
+   const file = target.files[0];
+});
+
+
+// на форму навешиваем событие
+// разблокируем кнопку
+// удаляем надпись "заполните все поля"
+modalSubmit.addEventListener('input', checkForm);
+
+// запрещаем перезагрузку при клике на "отправить"
+modalSubmit.addEventListener('submit', event => {
+   event.preventDefault();
+   // в этот объект добавляем свойства и значения из name/value
+   const itemObj = {};
+   // перебираем каждый элемент в форме Подать объявление"
+   for (const elem of elementsModalSubmit) {
          
-      }
-   });
+      itemObj[elem.name] = elem.value;
+   }
+      // это для проверки
+      // console.log(elem.name);
+      // console.log(elem.value);
+      // console.log(itemObj);
+         
+      // полученные элементы сохраняем в массив dataBase
+      dataBase.push(itemObj);
+      // закрывается модальное окно после нажатия на "отправить"
+      closeModal({target: modalAdd});
+      // и очищаем форму
+      // modalSubmit.reset();
+      console.log(dataBase);
+      // сохряняем введенные данные при клике на "отправить"
+      saveDB();
+});
 
-   // открываем модальное окно
-   addAd.addEventListener('click', () => {
-      modalAdd.classList.remove('hide');
-      //тут же блокируем кнопку "отправить"
-      modalBtnSubmit.disabled = true;
-      // закрываем попап по клику на Esc
-      document.addEventListener('keydown', closeModalEsc);
-   });
+// открываем модальное окно
+addAd.addEventListener('click', () => {
+   modalAdd.classList.remove('hide');
+   //тут же блокируем кнопку "отправить"
+   modalBtnSubmit.disabled = true;
+   // закрываем попап по клику на Esc
+   document.addEventListener('keydown', closeModal);
+});
 
-   // закрываем модальное окно первое
-   modalAdd.addEventListener('click', closeModal);
-   // закрываем модальное окно второе
-   modalItem.addEventListener('click', closeModal);
+catalog.addEventListener('click', event => {
+   // устанавливаем цель клика в каталоге
+   //картинка, заголовок, цена...
+   const target = event.target;
 
-   catalog.addEventListener('click', event =>{
-      // устанавливаем цель клика в каталоге
-      //картинка, заголовок, цена...
-      const target = event.target;
+   // если при клике добираемся до 'card'...
+   if (target.closest('.card')) {
+      // ...то убираем у модального окна класс 'hide'
+      modalItem.classList.remove('hide');
+      document.addEventListener('keydown', closeModal);
+   }
+});
 
-      // если при клике добираемся до 'card'...
-      if (target.closest('.card')) {
-         // ...то убираем у модального окна класс 'hide'
-         modalItem.classList.remove('hide');
-         document.addEventListener('keydown', closeModalEsc);
-      }
-   });
+// закрываем модальное окно первое
+modalAdd.addEventListener('click', closeModal);
+// закрываем модальное окно второе
+modalItem.addEventListener('click', closeModal);
 
    // закрываем модальное окно товаров
-   // этот вариант мы засунули в this
+   // это 
    // modalItem.addEventListener('click', event => {
    //    const target = event.target;
 
